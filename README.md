1342e17b413fc34f045835330133590118a3b4a4
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-11-10 09:17:53 -0300

    starting project

---
+++ README.md
@@ -0,0 +1,7 @@
+npm create vite@latest
+
+npm install -D tailwindcss postcss autoprefixer
+
+npx tailwindcss init -p
+
+npx sb init --builder @storybook/builder-vite --use-npm
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App() {
+
+
+  return (
+    <>
+      <h1 className='font-bold text-5xl text-violet-500' >Hello Word</h1>
+
+    </>
+  )
+}
+
+
