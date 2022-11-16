c9ff5d5bdc73dc599f94fca8f7cd3768b750a7a9
Leonardo Nogueira <leonardo.ngra@gmail.com>
2022-11-16 09:07:59 -0300

    update checkbox

--- src/components/Checkbox.stories.tsx
+++ src/components/Checkbox.stories.tsx
@@ -1,23 +1,22 @@
-import { Checkbox, CheckboxProps } from './CheckBox';
 import { Meta, StoryObj } from '@storybook/react'
-import { Text } from './Text';
+import { Checkbox, CheckboxProps } from './Checkbox'
+import { Text } from './Text'
 
 export default {
-    title: 'Components/Checkbox',
-    component: Checkbox,
-    args: {},
-    argTypes: { },
-    decorators: [
-        (Story) => {
-            return (
-                <div className='flex items-center gap-2'>
-                    {Story()}
-                    <Text size='sm'>Lembrar-me de mim por 30 dias </Text>
-                </div>
-            )
-        }
-    ]
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  args: {},
+  argTypes: {},
+  decorators: [
+    (Story) => {
+      return (
+        <div className="flex items-center gap-2">
+          {Story()}
+          <Text size="sm">Lembrar-me de mim por 30 dias</Text>
+        </div>
+      )
+    }
+  ],
 } as Meta<CheckboxProps>
 
 export const Default: StoryObj<CheckboxProps> = {}
-
